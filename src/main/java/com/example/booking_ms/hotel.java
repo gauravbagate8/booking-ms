7890e58506f28d5e5f307db2418304a57e30a218
Server <gaurav.bagate@gmail.com>
2025-06-30 21:28:43 +0530

    testing token

--- src/main/java/com/example/booking_ms/hotel.java
+++ src/main/java/com/example/booking_ms/hotel.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class hotel {
     @GetMapping("/hotel")
-    public String getData() {return  "Please  book flights Gaurav today itself" ; }
+    public String getData() {return  "Please  book flights Gaurav today itsel sladsasdas ; }
 }
