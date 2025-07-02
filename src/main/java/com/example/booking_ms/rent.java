9aaa462f90138c31458f0de0444ef28dc99efb7b
Server <gaurav.bagate@gmail.com>
2025-07-02 18:15:51 +0530

    testing pipeline with webhook

--- src/main/java/com/example/booking_ms/rent.java
+++ src/main/java/com/example/booking_ms/rent.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class rent {
     @GetMapping("/rent")
-    public String getData() {return  "Please  pay rent Gaurav Bagate today itsel"  ; }
+    public String getData() {return  "Please  pay rent sir today itsel"  ; }
 }
